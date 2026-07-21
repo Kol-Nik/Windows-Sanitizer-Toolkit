8f86cd04b50878e5b9ca92decee78847b5e4318f
Kol-Nik <kolev.nicky@gmail.com>
2026-07-21 22:17:23 +0300

    Modify Windows repair script for clarity and pause
    Updated title and added pause at the end of the script.

--- Maintenance/2-Fix-Windows.bat
+++ Maintenance/2-Fix-Windows.bat
@@ -3,7 +3,7 @@ chcp 65001 >nul
 net session >nul 2>&1
 if %errorLevel% NEQ 0 (echo Please run as Administrator! & pause & exit /b)
 
-title [2/6] Windows System Repair
+title [Maintenance]  Windows System Repair
 echo === STARTING SYSTEM REPAIR ===
 echo.
 echo [Step 1/2] Restoring system image (DISM)...
