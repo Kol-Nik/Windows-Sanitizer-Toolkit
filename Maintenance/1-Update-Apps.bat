1695b35c25d41436beee2f81e57909a4780f0844
Kol-Nik <kolev.nicky@gmail.com>
2026-07-21 22:16:53 +0300

    Modify update script for better user experience
    Updated batch script title and added pause at the end.

--- Maintenance/1-Update-Apps.bat
+++ Maintenance/1-Update-Apps.bat
@@ -1,6 +1,6 @@
 ﻿@echo off
 chcp 65001 >nul
-title [1/6] Software Updates
+title [Maintenance] Software Updates
 echo === CHECKING AND UPDATING INSTALLED APPLICATIONS ===
 echo Please wait...
 echo.
